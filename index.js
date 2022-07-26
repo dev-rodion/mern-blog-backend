8bc3d9f91cef3985df6ec606ba5005abbdc435ce
dev-rodion <gudkov.rodion.21@gmail.com>
2022-07-26 13:15:51 +0200

    add jsonwebtoken & add mongoose and connect to database

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+const mongoUrl = "mongodb://localhost:27017";
 const app = express();
 const port = 4444;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("DB is OK");
+  })
+  .catch((err) => console.log("DB error ", err));
 
 app.use(express.json());
 
@@ -10,8 +19,18 @@ app.get("/", (req, res) => {
 });
 
 app.post("/auth/login", (req, res) => {
+  const token = jwt.sign(
+    {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    },
+    "Strong Hash :D"
+  );
+
   res.json({
     success: true,
+    token,
   });
 });
 
