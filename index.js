ccbdfc6b838498ff79893e6116310eaca44625a2
dev-rodion <gudkov.rodion.21@gmail.com>
2022-07-26 14:56:11 +0200

    add express-validation and bcrypt

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
-const mongoUrl = "mongodb://localhost:27017";
+import bcrypt from "bcrypt";
+import { registerValidation } from "./validation/auth.js";
+import { validationResult } from "express-validator";
+
+import UserModel from "./models/User.js";
+
+const mongoUrl = "mongodb://127.0.0.1:27017";
 const app = express();
 const port = 4444;
 
@@ -18,20 +24,28 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/auth/login", (req, res) => {
-  const token = jwt.sign(
-    {
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    },
-    "Strong Hash :D"
-  );
+app.post("/auth/login", (req, res) => {});
 
-  res.json({
-    success: true,
-    token,
+app.post("/auth/register", registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    username: req.body.username,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash: passwordHash,
   });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(port, (err) => {
